ee9c3372af2f2615aadc4a94da6788f5900f8af7
busec0 <bulzan.sergiu@gmail.com>
2024-01-09 20:01:48 +0200

    Android Transports - WIP to request permissions

--- DittoMauiTasksApp/ViewModels/MainPageViewModel.cs
+++ DittoMauiTasksApp/ViewModels/MainPageViewModel.cs
@@ -1,8 +1,16 @@
 ﻿
 using System.Collections.ObjectModel;
 using System.Text.Json;
+#if ANDROID
+using Android.Content;
+using Android.Locations;
+using Android.Widget;
+#endif
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+#if IOS
+using CoreLocation;
+#endif
 using DittoMauiTasksApp.Utils;
 using DittoSDK;
 
@@ -16,13 +24,77 @@ namespace DittoMauiTasksApp.ViewModels
         [ObservableProperty]
         ObservableCollection<DittoTask> tasks;
 
+        DittoPresenceObserver obs;
+
         public MainPageViewModel(Ditto ditto, IPopupService popupService)
         {
             this.ditto = ditto;
             this.popupService = popupService;
 
-            ObserveDittoTasksCollection();
+            CheckPermissions().ContinueWith(async t =>
+            {
+                var result = await t;
+
+                ditto.DisableSyncWithV3();
+                ditto.TransportConfig.PeerToPeer.Awdl.Enabled = false;
+                ditto.StartSync();
+
+                ObserveDittoTasksCollection();
+            });
+
+            obs = this.ditto.Presence.Observe(s =>
+            {
+                System.Diagnostics.Debug.WriteLine(" - - - ");
+                foreach (var peer in s.LocalPeer.Connections)
+                {
+                    System.Diagnostics.Debug.WriteLine($"{peer.ConnectionType} - {BitConverter.ToString(peer.Peer1)} - {BitConverter.ToString(peer.Peer2)}");
+                }
+            });
         }
+
+        private async Task<bool> CheckPermissions()
+        {
+            PermissionStatus bluetoothStatus = await CheckBluetoothPermissions();
+
+            return  IsGranted(bluetoothStatus);
+        }
+
+        private async Task<PermissionStatus> CheckBluetoothPermissions()
+        {
+            PermissionStatus bluetoothStatus = PermissionStatus.Granted;
+
+            if (DeviceInfo.Platform == DevicePlatform.Android)
+            {
+                if (DeviceInfo.Version.Major >= 12)
+                {
+                    bluetoothStatus = await CheckPermissions<NearbyDevicesPermission>();
+                }
+                else
+                {
+                    bluetoothStatus = await CheckPermissions<Permissions.LocationWhenInUse>();
+                }
+            }
+
+            return bluetoothStatus;
+        }
+
+        private async Task<PermissionStatus> CheckPermissions<TPermission>() where TPermission : Permissions.BasePermission, new()
+        {
+            PermissionStatus status = await Permissions.CheckStatusAsync<TPermission>();
+
+            if (status != PermissionStatus.Granted)
+            {
+                status = await Permissions.RequestAsync<TPermission>();
+            }
+
+            return status;
+        }
+
+        private static bool IsGranted(PermissionStatus status)
+        {
+            return status == PermissionStatus.Granted || status == PermissionStatus.Limited;
+        }
+
 
         [RelayCommand]
         private async Task AddTaskAsync()
